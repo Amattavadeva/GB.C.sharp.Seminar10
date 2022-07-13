91d70e1f32ad3932b2ee9f6009b459361f78515e
Alex QAsper <semidog@gmail.com>
2022-07-13 11:36:29 +0300

    72 task

--- Program.cs
+++ Program.cs
@@ -34,9 +34,3 @@ for (int i = 0; i < info.Length; i++) // преобразуем получивш
     }
     k += info[i];
 }
-Console.WriteLine();
-
-for (int i = 0; i < result.Length; i++)
-{
-    Console.Write(result[i] + " ");
-}
